952d3887cff6b0bf72089b99cc9c6e0c3780b649
DiegoFabbrii <diegofabbri@outlook.com>
2023-01-20 18:24:29 -0300

    feat: static

---
+++ src/POO/Static/Static.ts
@@ -0,0 +1,21 @@
+export class Person {
+  static completeName = 'Diego Fabbri';
+
+  constructor(
+    public _name: string,
+    public _lastName: string,
+    public _age: number,
+  ) {}
+
+  static createPerson() {
+    return new Person('Luan', 'Brito', 0);
+  }
+
+  showStaticProperties() {
+    console.log(Person.completeName);
+  }
+}
+
+const person = new Person('Diego', 'Fabbri', 20);
+const person2 = Person.createPerson();
+person.showStaticProperties();
