80956b7102c1a0723eb59056e9a66facd7a9f280
DiegoFabbrii <diegofabbri@outlook.com>
2023-01-24 19:47:26 -0300

    feat: generics intersection

---
+++ src/generics/intersection/index.ts
@@ -0,0 +1,9 @@
+export function mergeObjs<O1, O2>(obj1: O1, obj2: O2): O1 & O2 {
+  //return { ...obj1, ...obj2 };
+  return Object.assign({}, obj1, obj2);
+}
+
+const obj1 = { chave1: 'valor1' };
+const obj2 = { chave2: 'valor2' };
+
+console.log(mergeObjs(obj1, obj2));
